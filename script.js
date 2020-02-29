201ac2570f34e900ab14b2d82b7246dd7a734ce4
dpbates13 <53348571+dpbates13@users.noreply.github.com>
2020-02-29 11:40:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,25 +6,11 @@ let albumDatabase = [];
 let artistDatabase = {};
 let genreList = [];
 let postedAlbums = [];
-let currentDate = new Date();
-let prevDate = new Date(currentDate - 604800000);
-let weekAgo = formatDate(prevDate);
+let albumCount = 0;
 let loaded = 0;
 let storeClick = "";
 
-function formatDate(date) {
-  let month = "" + (date.getMonth() + 1);
-  let day = "" + date.getDate();
-  let year = date.getFullYear();
-
-  if (month.length < 2) month = "0" + month;
-  if (day.length < 2) day = "0" + day;
-
-  return [year, month, day].join("-");
-}
-
 function getIds(url) {
-  //console.log('getIds GO');
   return new Promise((resolve, reject) =>
     fetch(url, {
       method: "GET",
@@ -35,23 +21,18 @@ function getIds(url) {
       }
     })
       .then(response => {
-        //console.log('getIds 1st response GO');
-
         if (response.status !== 200) {
           if (response.status == 404 || 401) {
             resolve(idList);
           } else {
             throw `${response.status}: ${response.statusText}`;
           }
-          //throw `${response.status}: ${response.statusText}`;
         }
         const r = response.json();
-        //console.log(r);
         return r;
       })
       .then(responseJson => {
-        //console.log('getIds 2nd response GO');
-        //let idList = [];
+        let idCount = 0;
         for (let i = 0; i < responseJson.albums.items.length; i++) {
           if (responseJson.albums.items[i] == null) {
             let z = 0;
@@ -59,28 +40,25 @@ function getIds(url) {
             idList.push(responseJson.albums.items[i].id);
           }
         }
-        //console.log(database);
+        idCount++;
         if (
           responseJson.albums.next !==
           "https://api.spotify.com/v1/search?query=tag%3Anew&type=album&offset=500&limit=50"
         ) {
           console.log(responseJson.albums.next);
           resolve(getIds(responseJson.albums.next));
-          //resolve();
         } else {
           resolve(idList);
         }
-        //resolve(idList);
       })
   );
 }
 
 function createIdArray(idList) {
   return new Promise((resolve, reject) => {
-    //console.log('createIdArray GO');
     let count = 0;
     let idString = "";
-    for (i = 0; i < idList.length; i++) {
+    for (let i = 0; i < idList.length; i++) {
       if (count == 19) {
         idString += `${idList[i]}`;
         idArray.push(idString);
@@ -91,7 +69,6 @@ function createIdArray(idList) {
         count++;
       }
     }
-    //console.log(idArray);
     resolve(idArray);
   });
 }
@@ -127,40 +104,23 @@ function createArtistStrings(data) {
   let strArr = [];
   let artStr = "";
   let count = 0;
-  //let tot = 0;
   console.log(data[0].albums[0].artists[0].id);
-  for (i = 0; i < data.length; i++) {
-    //console.log('go');
-    //tot ++;
-    //console.log(tot);
-    for (j = 0; j < data[i].albums.length; j++) {
-      //console.log('Go');
-      //console.log(data[i].albums[j].artists);
-      //console.log(data[i].albums[j]);
-      //console.log(`i=${i} and j=${j}`);
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].albums.length; j++) {
       let releaseDate = new Date(data[i].albums[j].release_date);
       let releaseMilli = releaseDate.getTime();
-      //console.log(releaseMilli);
       if (data[i].albums[j] === null) {
         let poiuoipu = 0;
-      }
-
-      if (releaseMilli < prevDate.getTime()) {
-        let wut = 0;
       } else {
         for (const artist in data[i].albums[j].artists) {
-          //console.log('GO!');
           if (count == 49) {
             artStr += `${data[i].albums[j].artists[artist].id}`;
-            //console.log(artStr);
             strArr.push(artStr);
             artStr = "";
             count = 0;
           } else {
             artStr += `${data[i].albums[j].artists[artist].id},`;
-            //console.log(artStr);
             count++;
-            //console.log(count);
           }
         }
       }
@@ -183,13 +143,10 @@ function getArtists(data, count = 0) {
       .then(response => {
         const r = response.json();
         console.log(r);
-        //resolve(r);
         return r;
       })
       .then(response => {
-        //console.log(`${response.artists[0].name}`);
-        for (i = 0; i < response.artists.length; i++) {
-          //console.log(response.artists[i].name);
+        for (let i = 0; i < response.artists.length; i++) {
           if (artistDatabase.hasOwnProperty(`${response.artists[i].name}`)) {
             let fdsjhf = 0;
           } else {
@@ -211,12 +168,8 @@ function addGenreData(genreList) {
   console.log("addGenreData Go!");
   console.log(albumDatabase[0].albums[0].genres);
   for (let i = 0; i < albumDatabase.length; i++) {
-    //console.log(albumDatabase[i]);
-    //console.log(albumDatabase[i].albums.length);
     for (let j = 0; j < albumDatabase[i].albums.length; j++) {
-      //console.log(albumDatabase[i].albums[j].artists);
       for (const artist in albumDatabase[i].albums[j].artists) {
-        //console.log(albumDatabase[i].albums[j].artists[artist].name);
         albumDatabase[i].albums[j].genres.push(
           `${genreList[`${albumDatabase[i].albums[j].artists[artist].name}`]}`
         );
@@ -228,7 +181,7 @@ function addGenreData(genreList) {
 
 function createGenreList() {
   for (const artist in artistDatabase) {
-    for (j = 0; j < artistDatabase[artist].length; j++) {
+    for (let j = 0; j < artistDatabase[artist].length; j++) {
       if (genreList.includes(artistDatabase[artist][j]) == false) {
         genreList.push(artistDatabase[artist][j]);
       }
@@ -265,39 +218,36 @@ function getToken() {
 }
 
 function displayGenreKeywords() {
-  /*
-  Display list of genre keywords.
-  On click, executes displayAlbums with that keyword as input.
-  */
   $(".loading").hide();
-  $(".keyGenres").append(`<h1>Please select a genre below</h1>`);
+  $(
+    `<h1>Please select a genre below</h1><p class="instructions">This will display albums associated with that genre, sorted by subgenre</p>`
+  ).insertBefore(".keyGenres");
   const keywords = [
     "hip hop",
-    "rap",
+    "r&b",
     "pop",
     "dance",
     "edm",
+    "electronic",
+    "trap",
     "rock",
-    "indie",
     "metal",
     "punk",
     "folk",
     "country",
-    "christian",
-    "electronic",
     "reggaeton",
     "latin",
     "classical",
-    "emo",
-    "trap"
+    "alternative",
+    "indie"
   ];
-  for (i = 0; i < keywords.length; i++) {
+  for (let i = 0; i < keywords.length; i++) {
     $(".keyGenres").append(`<li class="key">${keywords[i]}</li>`);
   }
   $(".keySearch").append(
     `<p>Or enter your own search term:</p>
-       <input id="text" type="text" value="">
-       <button type="submit" class "submit">Search genres</button>`
+       <input id="text" type="text" value="" aria-label="Genre Search">
+       <button type="submit" class="submit">Search genres</button>`
   );
   $(".keyGenres").on("click", ".key", function(event) {
     if (loaded == 0) {
@@ -321,21 +271,18 @@ function displayGenreKeywords() {
     } else {
       postedAlbums = [];
       $(".showAlbums").empty();
-      let userGenre = $("#text").val();
-      displayAlbums(userGenre);
+      let userGenre = $("#text")
+        .val()
+        .toLowerCase();
+      displayAlbums(userGenre.replace("'", ""));
       console.log(userGenre);
     }
   });
 }
 
 function displayAlbums(keyword = "rock") {
-  /*
-  Sort through genre list with keyword, create list of genres that have that keyword.
-  For each genre in list, executes generateAlbumsElement using that genreTerm.
-  Make sure to eliminate doubles.
-  Displays all album elements that were generated.
-  */
-  const re = new RegExp(keyword);
+  albumCount = 0;
+  const re = new RegExp(keyword.replace("amp;", ""));
   let listFromKeyword = [];
   for (i = 0; i < genreList.length; i++) {
     if (genreList[i].search(re) > -1) {
@@ -343,106 +290,88 @@ function displayAlbums(keyword = "rock") {
     }
   }
   for (p = 0; p < listFromKeyword.length; p++) {
-    if (listFromKeyword[p] == keyword) {
+    if (listFromKeyword[p] == keyword.replace("amp;", "")) {
       listFromKeyword.splice(p, 1);
     }
   }
   $(".showAlbums").append(
-    `<h2>Select a subgenre to sort by that subgenre</h2>`
+    `<h2>Select a subgenre to sort by that subgenre</h2><ul class="subGenres"></ul><h2 class="allAlbums">Albums</h2><p class="instructions">Click an albums title to open that album in Spotify</p>`
   );
+
+  for (k = 0; k < listFromKeyword.length; k++) {
+    $(".subGenres").append(`<li class="sub">${listFromKeyword[k]}</li>`);
+  }
+
+  $(".subGenres").on("click", ".sub", function(event) {
+    albumCount = 0;
+    postedAlbums = [];
+    $(".genreDiv").remove();
+    generateAlbumsElement($(this).html());
+  });
+
   listFromKeyword.push(keyword);
   console.log(listFromKeyword);
 
   for (m = 0; m < listFromKeyword.length; m++) {
     generateAlbumsElement(listFromKeyword[m]);
   }
+
+  if (albumCount == 0) {
+    $(".showAlbums").append(`<p class="noResults">No results</p>`);
+  }
 }
 
 function generateAlbumsElement(genreTerm = "yacht rock") {
-  /*
-  Search through albumDatabase for albums with genreTerm.
-  Create HTML element that displays genreTerm as header with albums and links below.
-  */
-  const re = new RegExp(genreTerm);
-  let genreClassA = genreTerm.replace("&", "n");
-  let genreCLassF = genreClassA.replace(" ", "-");
-  let genreClass = genreCLassF.replace(" ", "-");
+  const re = new RegExp(genreTerm.replace("amp;", ""));
+  console.log(re);
+  console.log(genreTerm);
+  let genreClassO = genreTerm.replace("&amp;", "&");
+  let genreClassA = genreClassO.replace("&", "n");
+  let genreCLassF = genreClassA.replace(/ /g, "-");
+  let genreClass = genreCLassF.replace("'", "");
   console.log(genreClass);
   $(".showAlbums").append(
-    `<div class="genreDiv ${genreClass}">
-            <h2 class="genreName">${genreTerm}</h2>
+    `<div class="genreDiv">
+            <h3 class="genreName">${genreTerm}</h3>
+            <div class="${genreClass} albumElements"></div>
         </div>`
   );
   for (i = 0; i < albumDatabase.length; i++) {
     for (j = 0; j < albumDatabase[i].albums.length; j++) {
       for (x = 0; x < albumDatabase[i].albums[j].genres.length; x++) {
-        //console.log(albumDatabase[i].albums[j].genres[x]);
         if (albumDatabase[i].albums[j].genres[x].search(re) > -1) {
-          //console.log(albumDatabase[i].albums[j].name);
-          //const id = new RegExp(albumDatabase[i].albums[j].id);
           if (postedAlbums.indexOf(albumDatabase[i].albums[j].id) == -1) {
             $(`.${genreClass}`).append(
-              `<img src="${albumDatabase[i].albums[j].images[1].url}">
+              `<div class="albumElement">
+                            <img src="${albumDatabase[i].albums[j].images[1].url}">
                             <a class="album" href="${albumDatabase[i].albums[j].uri}">${albumDatabase[i].albums[j].name}</a>
-                            <p class="artist">${albumDatabase[i].albums[j].artists[0].name}</p>`
+                            <p class="artist">${albumDatabase[i].albums[j].artists[0].name}</p>
+                            </div>`
             );
+            albumCount++;
             postedAlbums.push(albumDatabase[i].albums[j].id);
           }
         }
       }
     }
   }
-  //console.log(postedAlbums);
-}
-
-function clickSubGenre() {
-  $(".showAlbums").on("click", ".genreName", function(event) {
-    postedAlbums = [];
-    $(".showAlbums").empty();
-    generateAlbumsElement($(this).html());
-  });
 }
 
 async function createInitialDatabase() {
   startButton();
   await getToken();
-  /*
-  $(".loading").append(
-    `<p class="loadProg">Getting album IDs... <img src="loader.gif" width="25"/></p>`
-  );
-  */
   const start = await getIds(url);
   const idStrings = await createIdArray(start);
-  /*
-  $(".loading").append(
-    `<p class="loadProg">Getting album information... <img src="loader.gif" width="25"/></p>`
-  );
-  */
   const dataB = await createAlbumDatabase(idStrings);
-  console.log(dataB);
-  /*
-  $(".loading").append(
-    `<p class="loadProg">Getting genre info... <img src="loader.gif" width="25"/></p>`
-  );
-  */
   const artistStrings = createArtistStrings(dataB);
-  console.log(artistStrings);
   const artistGenres = await getArtists(artistStrings);
-  console.log(artistGenres);
   const finalDatabase = addGenreData(artistGenres);
-  console.log(finalDatabase);
   createGenreList();
-  console.log(genreList);
-  //displayGenreKeywords();
-  //clickSubGenre();
-  //displayAlbums();
-  //generateAlbumsElement();
 }
 
 function startButton() {
   $(".loading").on("click", "button", function(event) {
     displayGenreKeywords();
-    clickSubGenre();
   });
 }
 
